6fab4b28d742cae53058e191500714c3232d4b52
Nicole Gomez <nicolegomezx3@gmail.com>
2023-03-17 05:53:31 -0400

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 6;
+
+let multiply = (num1 * num2);
+
+let mod = num3 % num4;
+
+let max = Math.max(4,8,12,16,20);
+
+let random = Math.floor(Math.random() * 10) + 1;
+Math.isInteger(random);
 
 // Write your code here
